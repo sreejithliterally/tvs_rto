99767aa06bc308cd178be5bbfc3b51e75b95b4d7
sreejithliterally <imsreejithofficial@gmail.com>
2024-09-20 23:41:46 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import models
 import database
-from api import admin, login, sales, customer, accounts, finance
+from api import admin, login, sales, customer, accounts, finance, rto
 from dotenv import load_dotenv
 
 
@@ -30,7 +30,7 @@ app.include_router(sales.router)
 app.include_router(customer.router)
 app.include_router(accounts.router)
 app.include_router(finance.router)
-
+app.include_router(rto.router)
 
 @app.get("/")
 async def root():
