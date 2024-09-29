730b8ecbc9a4748c4144743e25c55bfea4e67f2e
sreejithliterally <imsreejithofficial@gmail.com>
2024-09-29 13:41:57 +0530

    get customer by id

--- app/api/sales.py
+++ app/api/sales.py
@@ -109,6 +109,44 @@ def get_customers_for_sales_executive(db: Session = Depends(database.get_db), cu
     return customer_data
 
 
+@router.get("/customers/{customer_id}", response_model=schemas.CustomerOut)
+def get_customer_by_id(customer_id: int, db: Session = Depends(database.get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+    if current_user.role_id != 2:
+
+        raise HTTPException(status_code=403, detail="Not authorized.")
+
+    customer = db.query(models.Customer).filter(
+        models.Customer.customer_id == customer_id,  
+        models.Customer.branch_id == current_user.branch_id,  
+        models.Customer.sales_executive_id == current_user.user_id  
+    ).first()
+
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found or you are not authorized to view this customer.")
+
+
+    customer_data = {
+        "customer_id": customer.customer_id,
+        "name": customer.name,
+        "first_name": customer.first_name,
+        "last_name": customer.last_name,
+        "address": customer.address,
+        "phone_number": customer.phone_number,
+        "status": customer.status,
+        "branch_id": customer.branch_id,
+        "photo_adhaar_front": customer.photo_adhaar_front,
+        "photo_adhaar_back": customer.photo_adhaar_back,
+        "photo_passport": customer.photo_passport,
+        "sales_verified": customer.sales_verified,
+        "accounts_verified": customer.accounts_verified,
+        "vehicle_name": customer.vehicle_name,
+        "vehicle_variant": customer.vehicle_variant,
+        "ex_showroom_price": customer.ex_showroom_price,
+        "tax": customer.tax
+    }
+
+    return customer_data
+
 @router.put("/customers/{customer_id}", response_model=CustomerResponse)
 def update_customer(customer_id: int, update_data: CustomerUpdatesales, db: Session = Depends(database.get_db)):
     # Fetch the customer from the database
